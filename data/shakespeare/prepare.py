d770d58761ce09f76a4c14c0ffd58bb8defcca28
Zhiheng Wang <zhi_heng.wang@ceasia.cn>
2023-01-29 12:53:43 +0800

    Added input.txt to the Shakespeare data set to avoid avoid downloading.

--- data/shakespeare/prepare.py
+++ data/shakespeare/prepare.py
@@ -1,15 +1,16 @@
 import os
-import requests
 import tiktoken
 import numpy as np
 
 # download the tiny shakespeare dataset
-input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
-if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-    with open(input_file_path, 'w') as f:
-        f.write(requests.get(data_url).text)
+#import requests
+#if not os.path.exists(input_file_path):
+#    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+#    with open(input_file_path, 'w') as f:
+#        f.write(requests.get(data_url).text)
 
+# added the input.txt to the repo
+input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
 with open(input_file_path, 'r') as f:
     data = f.read()
 n = len(data)
